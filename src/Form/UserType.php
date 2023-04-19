607428776afe7827b12947870d9b2ce63e4d7776
Gabriel <gabriel.randrianaridera@etu.univ-poitiers.fr>
2023-04-19 16:57:47 +0200

    web

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,21 +3,19 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Collection;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('login',
-                        TextType::class,
-                        [
-                            'label' => 'Login',
-            ])
-
+            ->add('login')
             ->add('roles')
             ->add('password')
             ->add('nom')
